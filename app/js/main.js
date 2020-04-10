6d245ce0af8ff750d1eed9e48e387985e96bad08
brendan8 <your_email@whatever.com>
2020-04-11 02:40:03 +0300

    Я пытался сделать слайдер, ПРАВДА пытался, но не смог. Нужно продолжить завтра

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
-
+    $('.body__slide-wrap').slick({
+        autoplay: false,
+        autoplaySpeed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: true,
+    });
 });
